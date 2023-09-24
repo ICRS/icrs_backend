3055009357bbcecc29f74ff058ac8b2447909dc8
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2023-09-24 17:21:26 +0100

    added union integration

---
+++ union.py
@@ -0,0 +1,11 @@
+from icu_ea_api import ICUEActivitiesAPI
+import os
+
+csp_code = 625
+api_key = os.getenv('API_KEY')
+year = '23-24'
+society_api = ICUEActivitiesAPI(csp_code, api_key, year)
+
+def isMember(shortcode: str) -> bool:
+    return shortcode in [member['Login'] for member in society_api.list_members()]
+
