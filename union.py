09acbfba2e0261630a6571ede1ec78823decdfca
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2023-09-24 22:26:30 +0100

    added functionality for mapping shortcodes to cids

--- union.py
+++ union.py
@@ -11,3 +11,6 @@ society_api = ICUEActivitiesAPI(csp_code, api_key, year)
 def isMember(shortcode: str) -> bool:
     return shortcode in [member['Login'] for member in society_api.list_members()]
 
+def getShortcodesToCIDAndName(shortcodes) -> list:
+    d = {member['Login']: (f"{member['FirstName']} {member['Surname']}", member['CID']) for member in society_api.list_members()}
+    return [d[s] for s in shortcodes if s in d]
