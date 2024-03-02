bea383074b4cdec635a9f9b809a83caa082424d5
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2024-03-02 21:56:34 +0000

    init postgres stuff

---
+++ src/database.py
@@ -0,0 +1,13 @@
+import psycopg2
+from configparser import ConfigParser
+
+config = ConfigParser()
+config.read('postgres.ini')
+
+DB_CONFIG = {
+    'database': config['postgres']['database'],
+    'user': config['postgres']['user'],
+    'password': config['postgres']['password'],
+    'host': config['postgres']['host'],
+    'port': config['postgres']['port']
+}
--- src/print_metrics.py
+++ src/print_metrics.py
@@ -1,16 +1,9 @@
-import sqlite3
-import datetime
-import os
 import tornado
 import json
 from tornado_swagger.model import register_swagger_model
+import psycopg2 as pg
 
-try:
-    env = os.environ["ENV"]
-except:
-    env = "dev"
-
-DATABASE = "/home/pi/code/icrs_security/database.db" if env != "dev" else "database.db"
+from src.database import DB_CONFIG
 
 @register_swagger_model
 class PrintStatistics(tornado.web.RequestHandler):
@@ -45,9 +38,9 @@ class PrintStatistics(tornado.web.RequestHandler):
               description: list of shortcodes, print durations and print weight
         """
         start_time = self.get_argument("start_time", default="1980-01-01")
-        with sqlite3.connect(DATABASE) as con:
-            cur = con.cursor()
-            cur.execute("SELECT SHORTCODE, SUM(PRINT_DURATION), SUM(PRINT_WEIGHT) FROM PRINT_METRICS WHERE TIME_STARTED > ? GROUP BY SHORTCODE", (start_time,))
-            self.write(json.dumps([{ "shortcode" : c[0], "print_duration": c[1], "print_weight": c[2]} for c in cur.fetchall()]))
+        with pg.connect(**DB_CONFIG) as conn:
+            with conn.cursor() as cur:
+                cur.execute("SELECT SHORTCODE, SUM(PRINT_DURATION), SUM(PRINT_WEIGHT) FROM PRINT_METRICS WHERE TIME_STARTED > ? GROUP BY SHORTCODE", (start_time,))
+                self.write(json.dumps([{ "shortcode" : c[0], "print_duration": c[1], "print_weight": c[2]} for c in cur.fetchall()]))
         
 
