2bcceca5f4c6d96eb5dfc4eae8976eb72669815d
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2024-03-03 01:21:49 +0000

    refactored class names

--- server/src/access_server.py
+++ server/src/access_server.py
@@ -151,7 +151,7 @@ class SetUserCanPrint(tornado.web.RequestHandler):
         return
  
 
-class setPrintWindow(tornado.web.RequestHandler):
+class SetPrintWindow(tornado.web.RequestHandler):
     '''verifies if a user can print, if yes a print window of default 1 min is opened'''
     def post(self):
         try:
@@ -190,12 +190,7 @@ class setPrintWindow(tornado.web.RequestHandler):
         self.write(msg)
         
 
-
-# @app.route('/getCanPrint', methods = ['GET'])
-# def get_can_print():
-
-#     return {"canPrint": last_set_time > datetime.datetime.now()}
-class getPrintWindow(tornado.web.RequestHandler):
+class GetPrintWindow(tornado.web.RequestHandler):
     '''queries if the print window is open, returns true if open'''
     def get(self):
         try:            
@@ -206,12 +201,12 @@ class getPrintWindow(tornado.web.RequestHandler):
             self.write("Error in post message")
 
 
-class getRegistrationPortal(tornado.web.RequestHandler):
+class GetRegistrationPortal(tornado.web.RequestHandler):
     def get(self):
         self.render("template.html")
 
 
-class getValidUsers(tornado.web.RequestHandler):
+class GetValidUsers(tornado.web.RequestHandler):
     def getValidNameCIDs(self):
         try:
             with pg.connect(**DB_CONFIG) as conn:
@@ -236,7 +231,7 @@ class getValidUsers(tornado.web.RequestHandler):
         except:
             self.write("ERROR")
 
-class getUserPerms(tornado.web.RequestHandler):
+class GetUserPerms(tornado.web.RequestHandler):
     def post(self):
         data = json.loads(self.request.body)
         uid = data.get('id').strip().replace(" ","")
@@ -259,7 +254,7 @@ class getUserPerms(tornado.web.RequestHandler):
         finally:
             conn.close()
 
-class printMetrics(tornado.web.RequestHandler):
+class PrintMetrics(tornado.web.RequestHandler):
     '''Saves metrics for a singe print job'''
     def parse_to_int(self, s: str) -> int:
         '''Expects the time to be in in seconds (float)'''
@@ -280,7 +275,7 @@ class printMetrics(tornado.web.RequestHandler):
 
         return
 
-class getMetrics(tornado.web.RequestHandler):
+class GetMetrics(tornado.web.RequestHandler):
     def post(self):
         data = json.loads(self.request.body)
         shortcode = data['shortcode'].lower()
--- server/src/server.py
+++ server/src/server.py
@@ -2,15 +2,15 @@ import tornado
 from src.access_server import (
     AddUser,
     SetUserCanPrint,
-    getMetrics,
+    GetMetrics,
     UpdateUser,
-    getRegistrationPortal,
+    GetRegistrationPortal,
     RegisterUsers,
-    setPrintWindow,
-    getPrintWindow,
-    getValidUsers,
-    getUserPerms,
-    printMetrics,
+    SetPrintWindow,
+    GetPrintWindow,
+    GetValidUsers,
+    GetUserPerms,
+    PrintMetrics,
 )
 from src.print_metrics import PrintStatistics
 from tornado_swagger.setup import setup_swagger
@@ -21,15 +21,15 @@ class Application(tornado.web.Application):
         tornado.web.url(r"/addUser",AddUser),
         tornado.web.url(r"/updateUser", UpdateUser),
         tornado.web.url(r"/setCanPrint", SetUserCanPrint),
-        tornado.web.url(r"/setPrintWindow", setPrintWindow),
-        tornado.web.url(r"/getPrintWindow", getPrintWindow),
-        tornado.web.url(r"/getRegistrationPortal", getRegistrationPortal),
+        tornado.web.url(r"/setPrintWindow", SetPrintWindow),
+        tornado.web.url(r"/getPrintWindow", GetPrintWindow),
+        tornado.web.url(r"/getRegistrationPortal", GetRegistrationPortal),
         tornado.web.url(r"/registerUsers", RegisterUsers),
-        tornado.web.url(r"/getValidUsers", getValidUsers),
-        tornado.web.url(r"/getUserPerms", getUserPerms),
-        tornado.web.url(r"/postPrintTime", printMetrics),
-        tornado.web.url(r"/printStatistics", PrintStatistics),    
-        tornado.web.url(r"/getMetrics", getMetrics),
+        tornado.web.url(r"/getValidUsers", GetValidUsers),
+        tornado.web.url(r"/getUserPerms", GetUserPerms),
+        tornado.web.url(r"/postPrintTime", PrintMetrics),
+        tornado.web.url(r"/printStatistics", PrintStatistics),
+        tornado.web.url(r"/getMetrics", GetMetrics),
     ]
 
     def __init__(self):
