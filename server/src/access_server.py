05ca13b4c5a97fff07aac1b8dca23a3a745f2c27
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2024-03-03 19:36:26 +0000

    add cors to add user endpoint

--- server/src/access_server.py
+++ server/src/access_server.py
@@ -39,6 +39,11 @@ def db_execute_command(sql_query, parameters):
         return msg
 
 class AddUser(tornado.web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*") # TODO: remove wildcard
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'POST')
+
     '''adds a user to db with given ID and perms'''
     def post(self):
         try:
