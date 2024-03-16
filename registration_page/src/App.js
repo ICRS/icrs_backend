1b3eab8bc1c47ba440b8dbae96109500e56914e4
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2024-03-16 02:01:27 +0000

    added update permission page and replaced secret verification with tornado auth

--- registration_page/src/App.js
+++ registration_page/src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 
 import {
-  Route,
-  Routes,
-  Navigate
+    Route,
+    Routes,
+    Navigate
 } from "react-router-dom";
 import Registration from './pages/Registration/Registration';
 import UserDetails from './pages/UserDetail/UserDetail';
@@ -13,25 +13,27 @@ import app_config from "./settings.json";
 import AllUsers from './pages/Users/AllUsers';
 import Login from './pages/Login/Login';
 import { useState } from 'react';
+import UpdatePermissions from './pages/UpdatePermissions/UpdatePermissions';
 
 function App() {
-  const [login, setLogin] = useState(false);
+    const [login, setLogin] = useState(false);
 
-  return (
-    <Routes>
-      <Route path="/login" element={<Login loginHandler={setLogin} />} />
-      {
-        login && 
-        <Route path="/" element={<Root />} >
-          <Route path="/registration" element={<Registration endpoint={app_config["FORM_ENDPOINT"]}  />} />
-          <Route path="/user" element={<UserDetails endpoint={app_config["USER_ENDPOINT"]}  />} />
-          <Route path="/users/all" element={<AllUsers endpoint={app_config["ALL_USERS_ENDPOINT"]}  />} />
-        </Route> 
-      }
-      <Route path="*" element={<Navigate to="/login" />} />
-    </Routes>
-    // <RouterProvider router={router} />
-  );
+    return (
+        <Routes>
+            <Route path="/login" element={<Login loginHandler={setLogin} endpoint={app_config["LOGIN_ENDPOINT"]} />} />
+            {
+                login &&
+                <Route path="/" element={<Root />} >
+                    <Route path="/registration" element={<Registration endpoint={app_config["FORM_ENDPOINT"]} />} />
+                    <Route path="/user/detail" element={<UserDetails endpoint={app_config["USER_ENDPOINT"]} />} />
+                    <Route path="/user/permission" element={<UpdatePermissions endpoint={app_config["UPDATE_PERMISSION_ENDPOINT"]} />} />
+                    <Route path="/users/all" element={<AllUsers endpoint={app_config["ALL_USERS_ENDPOINT"]} />} />
+                </Route>
+            }
+            <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
+        // <RouterProvider router={router} />
+    );
 }
 
 export default App;
