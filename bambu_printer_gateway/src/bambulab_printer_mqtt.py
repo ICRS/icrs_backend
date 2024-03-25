3b5b7ea4a90605fa54e3ee365ed126943e8c3d25
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2024-03-25 22:55:43 +0000

    fixed formatting

--- bambu_printer_gateway/src/bambulab_printer_mqtt.py
+++ bambu_printer_gateway/src/bambulab_printer_mqtt.py
@@ -3,25 +3,26 @@ import ssl
 
 import paho.mqtt.client as mqtt
 
+
 class PrinterMQTTClient:
     def __init__(self, hostname, access, printer_serial, username="bblp", port=8883, timeout=60):
         self._hostname = hostname
         self._access = access
         self._username = username
         self._printer_serial = printer_serial
-        
+
         self._port = port
         self._timeout = timeout
-        
+
         self._client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
         self._client.username_pw_set(username, access)
-        self._client.tls_set(tls_version=ssl.PROTOCOL_TLS, cert_reqs=ssl.CERT_NONE)
+        self._client.tls_set(tls_version=ssl.PROTOCOL_TLS,
+                             cert_reqs=ssl.CERT_NONE)
         self._client.tls_insecure_set(True)
-
 
         self._client.on_connect = self._on_connect
         self._client.on_message = self._on_message
-        
+
         self._data = {}
 
     def _on_message(self, client, userdata, msg) -> None:  # pylint: disable=unused-argument  # noqa
@@ -29,11 +30,11 @@ class PrinterMQTTClient:
         doc = json.loads(msg.payload)
 
         # print(doc)
-        
+
         if "print" in doc:
             self._data |= doc["print"]
-            print(self._data) 
-    
+            print(self._data)
+
     def _on_connect(self, client, serial, userdata, flags, rc) -> None:  # pylint: disable=unused-argument  # noqa
         """
         _on_connect Callback function for when the client
@@ -56,31 +57,32 @@ class PrinterMQTTClient:
 
     def connect(self):
         self._client.connect_async(self._hostname, self._port, self._timeout)
-    
+
     def start(self):
         self._client.loop_start()
-        
+
     def loop_forever(self):
         self._client.loop_forever()
 
     def stop(self):
         self._client.loop_stop()
-    
+
     def get_last_print_percentage(self):
         return self._data.get("mc_percent", None)
-    
+
     def get_remaining_time(self):
         return self._data.get("mc_remaining_time", None)
-    
+
     def get_printer_state(self):
-        return self._data.get("gcode_state", "IDLE") 
+        return self._data.get("gcode_state", "IDLE")
 
     def get_file_name(self) -> str:
         return self._data.get("gcode_file", "")
-    
+
     def get_print_speed(self) -> int:
         return int(self._data.get("spd_mag", 100))
-    
+
+
 """
 Example Use case
 
