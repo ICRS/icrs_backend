c01c6d006348602626f6d32a2da74f482374349d
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2024-05-03 23:21:06 +0100

    updated server; removed bambu configs

--- auto_slicer/scripts/download.sh
+++ auto_slicer/scripts/download.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 wget -O bambu.zip https://github.com/bambulab/BambuStudio/archive/refs/tags/v01.09.01.58.zip  
 
 unzip bambu.zip  "BambuStudio-01.09.01.58/resources/profiles/BBL/*"
--- auto_slicer/src/server.py
+++ auto_slicer/src/server.py
@@ -1,8 +1,9 @@
 import logging
 import subprocess
 
-from typing import BinaryIO
-from fastapi import APIRouter, UploadFile
+from fastapi import APIRouter
+import requests
+
 
 from .printer_asset_utils import AVAILABLE_LAYER_HEIGHT, AVAILABLE_PRINTERS, \
     get_machine, process_from_machine_layer, printer_pla
@@ -19,33 +20,49 @@ router = APIRouter()
 
 
 def slice_file_bin(
-    file: BinaryIO,
+    file_url: str,
     filename: str,
     filament_path: str = "assets/filament/Generic PLA.json",
     machine_path: str = "assets/machine/Bambu Lab P1P 0.4 nozzle.json",
     process_path: str = "assets/process/0.28mm Extra Draft @BBL P1P.json",
 ) -> None:
-    file.write(filename)
-    subprocess.run(["./bambu-studio"])
+    with requests.get(file_url) as f:
+        with open(filename, "wb") as file:
+            file.write(f.content)
+
+    subprocess.run(["./bambu-studio",
+                    "--curr-bed-type", "Textured PEI Plate",
+                    "--avoid-extrusion-cali-region",
+                    "--allow-rotations",
+                    "--avoid-extrusion-cali-region",
+                    "--orient", 1,
+                    "--arrange", 1,
+                    "--load-settings",
+                    f"{machine_path};{process_path}",
+                    "--load-filaments", filament_path,
+                    "--ensure-on-bed",
+                    "--slice", 1,
+                    "--output-dir", f"tmp/{filename}",
+                    "--load-settings", f"{process_path};{machine_path}",
+                    filename])
 
 
 @router.post("/slice/file")
 async def slice_file(
-        file: UploadFile,
+        file_url: str,
+        file_name: str,
         printer_type: str = AVAILABLE_PRINTERS,
         layer_height: float = AVAILABLE_LAYER_HEIGHT,):
     try:
-        io_file: BinaryIO = file.file
-
         filament_file_name = printer_pla(printer_type)
         process_file_name = process_from_machine_layer(
             machine_type=printer_type,
             layer_height=layer_height)
         machine_file_name = get_machine(printer_type)
 
-        if file.filename:
-            slice_file_bin(io_file,
-                           file.filename,
+        if file_url:
+            slice_file_bin(file_url,
+                           file_name,
                            filament_file_name=filament_file_name,
                            process_path=process_file_name,
                            machine_path=machine_file_name)
