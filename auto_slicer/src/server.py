0dfe2e40778dbafd55d0aeb85eeb6d3f4ab8a841
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2024-05-03 21:23:16 +0100

    moved code to printer utils

--- auto_slicer/src/server.py
+++ auto_slicer/src/server.py
@@ -1,10 +1,11 @@
-from functools import cache
 import logging
-import os
 import subprocess
 
 from typing import BinaryIO
-from fastapi import APIRouter, Query, UploadFile
+from fastapi import APIRouter, UploadFile
+
+from .printer_asset_utils import AVAILABLE_LAYER_HEIGHT, AVAILABLE_PRINTERS, \
+    get_machine, process_from_machine_layer, printer_pla
 
 
 logging.basicConfig(level=logging.INFO,
@@ -16,9 +17,6 @@ logging.basicConfig(level=logging.INFO,
 
 router = APIRouter()
 
-AVAILABLE_PRINTERS = Query(enum=["p1p", "p1s"])
-AVAILABLE_LAYER_HEIGHT = Query(enum=[0.12, 0.16, 0.20, 0.24, 0.28])
-
 
 def slice_file_bin(
     file: BinaryIO,
@@ -29,94 +27,6 @@ def slice_file_bin(
 ) -> None:
     file.write(filename)
     subprocess.run(["./bambu-studio"])
-
-
-@cache
-def available_processes() -> list[str]:
-    return os.listdir("assets/process")
-
-
-@cache
-def available_machine() -> list[str]:
-    return os.listdir("assets/machine")
-
-
-@cache
-def p1p_processes() -> list[str]:
-    return sorted(
-        [s for s in available_processes() if "P1P" in s], reverse=True)
-
-
-@cache
-def x1c_processes() -> list[str]:
-    return sorted(
-        [s for s in available_processes() if "X1C" in s], reverse=True)
-
-
-@cache
-def p1p_process_from_layer_height(layer_height: float) -> str:
-    layer_height = "%.2f" % layer_height
-    layer_prefix = f"{layer_height}mm"
-    return next((s for s in p1p_processes() if layer_prefix in s),
-                p1p_processes()[0])
-
-
-@cache
-def x1c_process_from_layer_height(layer_height: float) -> str:
-    layer_height = "%.2f" % layer_height
-    layer_prefix = f"{layer_height}mm"
-    return next((s for s in x1c_processes() if layer_prefix in s),
-                x1c_processes()[0])
-
-
-p1s_processes = x1c_processes
-p1s_process_from_layer_height = x1c_process_from_layer_height
-
-
-@cache
-def process_from_machine_layer(machine: str, layer_height: float) -> str:
-    if machine.lower() == "p1p":
-        return p1p_process_from_layer_height(layer_height)
-    elif machine.lower() == "p1s":
-        return p1s_process_from_layer_height(layer_height)
-    return x1c_process_from_layer_height(layer_height)
-
-
-@cache
-def get_machine(machine_type: str = AVAILABLE_PRINTERS) -> str:
-    machine_type = machine_type.lower()
-    return next((s for s in available_machine() if machine_type in s.lower()),
-                "Bambu Lab P1P 0.4 nozzle.json")
-
-
-@router.get("/processes")
-async def get_processes():
-    return available_processes()
-
-
-@router.get("/processes/p1p")
-async def get_p1p_processes():
-    return p1p_processes()
-
-
-@router.get("/processes/p1s")
-async def get_p1s_processes():
-    return p1s_processes()
-
-
-@router.get("/process/p1p")
-async def get_p1p_process(layer_height: float = AVAILABLE_LAYER_HEIGHT):
-    return p1p_process_from_layer_height(layer_height)
-
-
-@router.get("/process/p1s")
-async def get_p1s_process(layer_height: float = AVAILABLE_LAYER_HEIGHT):
-    return p1s_process_from_layer_height(layer_height)
-
-
-@router.get("/machine")
-async def machine(machine_type: str = AVAILABLE_PRINTERS):
-    return get_machine(machine_type)
 
 
 @router.post("/slice/file")
@@ -142,9 +52,3 @@ async def slice_file(
     except Exception as e:
         logging.exception(f"Slice file failed: {e}")
         pass
-
-
-def printer_pla(printer_type: str) -> str:
-    if printer_type.lower() == "p1p":
-        return "Generic PLA @BBL P1P.json"
-    return "Generic PLA.json"
