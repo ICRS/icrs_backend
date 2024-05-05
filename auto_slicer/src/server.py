e7642eb92b36c645263ec3dcb0cb2553d81e976c
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2024-05-05 15:08:06 +0100

    added folder generation stuff; updated slice file api; added delete file skeletons

--- auto_slicer/src/server.py
+++ auto_slicer/src/server.py
@@ -4,7 +4,7 @@ import subprocess
 import requests
 
 import xml.etree.ElementTree as ET
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 
 from .printer_asset_utils import AVAILABLE_LAYER_HEIGHT, AVAILABLE_PRINTERS, \
     get_machine, process_from_machine_layer, printer_pla
@@ -46,18 +46,45 @@ def weight(filename: str) -> float:
     )
 
 
+def delete_file(file_path: str) -> bool:
+    raise NotImplementedError
+
+
+def generate_foldername(filename: str, url: str, shortcode: str) -> str:
+    """
+    Generates the folder name for storing temporary files,
+    gcode outputs and 3mfs
+
+    Args:
+        filename (str): filename of the current file
+        url (str): url/discord endpoint to download the file from
+        shortcode (str): shortcode of the user
+
+    Returns:
+        str: shortcode + url_params + filename
+    """
+    return "_".join([shortcode, url.split("?"), filename])
+
+
 def slice_file_bin(
-    file_url: str,
+    url: str,
     filename: str,
+    shortcode: str,
     filament_path: str = "assets/filament/Generic PLA.json",
     machine_path: str = "assets/machine/Bambu Lab P1P 0.4 nozzle.json",
     process_path: str = "assets/process/0.28mm Extra Draft @BBL P1P.json",
 ) -> None:
-    with requests.get(file_url) as f:
-        with open(filename, "wb") as file:
+    folder_name = "tmp/" + generate_foldername(
+        filename=filename,
+        url=url,
+        shortcode=shortcode)
+
+    os.makedirs(folder_name, exist_ok=True)
+    temporary_file_path = f"{folder_name}/{filename}"
+    with requests.get(url) as f:
+        with open(temporary_file_path, "wb") as file:
             file.write(f.content)
 
-    os.makedirs(f"tmp/{filename}", exist_ok=True)
     command = ["./bambu-studio",
                '--curr-bed-type', 'Textured PEI Plate',
                "--avoid-extrusion-cali-region",
@@ -70,21 +97,27 @@ def slice_file_bin(
                "--load-filaments", f"\"{filament_path}\"",
                "--ensure-on-bed",
                "--slice", "1",
-               "--outputdir", f"tmp/{filename}",
+               "--outputdir", folder_name,
                "--export-3mf", "output.3mf",
-               filename]
+               temporary_file_path]
 
     logging.info(command)
     result = subprocess.run(command)
+
+    os.remove(temporary_file_path)
+
     return result
 
 
 @router.post("/slice/file")
 async def slice_file(
-        file_url: str,
-        file_name: str,
+        shortcode: str,
+        filename: str,
+        url: str,
+        layer_height: float = AVAILABLE_LAYER_HEIGHT,
+        infill: int = Query(15, ge=5, le=30),
         printer_type: str = AVAILABLE_PRINTERS,
-        layer_height: float = AVAILABLE_LAYER_HEIGHT,):
+        ):
     try:
         filament_file_name = printer_pla(printer_type)
         process_file_name = process_from_machine_layer(
@@ -92,24 +125,25 @@ async def slice_file(
             layer_height=layer_height)
         machine_file_name = get_machine(printer_type)
 
-        if file_url:
+        if url:
             logging.info(f"{filament_file_name}")
             logging.info(f"{process_file_name}")
             logging.info(f"{machine_file_name}")
             result = slice_file_bin(
-                file_url,
-                file_name,
+                shortcode=shortcode,
+                url=url,
+                filename=filename,
                 filament_path="assets/filament/"+filament_file_name,
                 process_path="assets/process/"+process_file_name,
                 machine_path="assets/machine/"+machine_file_name)
             logging.debug(f"Slicing Result: {result}")
 
             model_time, estimated_time = gcode_time(
-                f"tmp/{file_name}/plate_1.gcode")
+                f"tmp/{filename}/plate_1.gcode")
 
             return {"slice_result": result,
-                    "file_name": file_name,
-                    "file_url": file_url,
+                    "filename": filename,
+                    "url": url,
                     "printer_type": printer_type,
                     "layer_height": layer_height,
                     "plates": 1,
@@ -119,3 +153,19 @@ async def slice_file(
     except Exception as e:
         logging.exception(f"Slice file failed: {e}")
         pass
+
+
+@router.post("/slice/release")
+async def release_file(
+        filename: str,
+        url: str,
+        shortcode: str = Query(max_length=7),
+        release: bool = True) -> dict:
+
+    with open(filename, "r") as f:
+        content = {}
+        content["content"] = f.read()
+
+        pass
+
+    return {}
