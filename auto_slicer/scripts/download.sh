56d58e3e4f8d418914089c6de1dfc9b2e1e21eea
MChrisGM <c.i.cristakos@gmail.com>
2024-05-07 11:14:53 +0100

    Fixed docker

--- auto_slicer/scripts/download.sh
+++ auto_slicer/scripts/download.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
 wget -O bambu.zip https://github.com/bambulab/BambuStudio/archive/refs/tags/v01.09.01.58.zip  
-
 unzip bambu.zip  "BambuStudio-01.09.01.58/resources/profiles/BBL/*"
 rm bambu.zip
-
 mv BambuStudio-01.09.01.58/resources/profiles/BBL BBL
 rm BambuStudio-01.09.01.58 -r
-
 python3 merge.py
-
 rm BBL -r
 exit 0
