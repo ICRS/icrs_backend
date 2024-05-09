aea43af36a4a5aa4f7b580cacabfa940c6fb4f13
Chris Ioannidis <142539198+acse-ci223@users.noreply.github.com>
2024-05-09 01:56:51 +0100

    Added queue functionality to send gcode and start prints

--- printer_queue_manager/src/queue_handler.py
+++ printer_queue_manager/src/queue_handler.py
@@ -1,3 +1,4 @@
+import io
 import json
 import logging
 import os
@@ -7,8 +8,11 @@ import sys
 import pika
 import pika.channel
 import pika.delivery_mode
+import bambulabs_api as blapi
+from fastapi import UploadFile
 
 from src.printer_farm import PrinterFarm
+from src.printer_gateway import PrinterGateway
 
 
 DEBUG = str(os.getenv('DEBUG', False)).lower() in ['true', '1']  # noqa  # pylint: disable=invalid-envvar-default
@@ -50,14 +54,14 @@ class QueueManager:
     
     def rabbit_callback(self, ch: pika.channel.Channel, method, properties, body: bytes):
         # Decode bytes sent from rabbitmq queue
-        data = body.decode()
+        data: str = body.decode()
         try:
             # Parse bytes to json and then to dict
-            request = dict(json.loads(data))
-            logging.info(f"Received {request}")
-        except json.JSONDecodeError:
+            json_data = json.loads(data)
+            request = dict(json_data)
+        except json.JSONDecodeError as e:
             # If data is not valid json, log error and reject message without requeueing (The object in the queue might be invalid)
-            logging.error(f"Received invalid data: {data}")
+            logging.error(f"Received invalid data: {data}: {str(e)}")
             ch.basic_reject(delivery_tag=method.delivery_tag, requeue=False)
             return
         
@@ -73,8 +77,9 @@ class QueueManager:
         
         # Queue the job to a printer. If the job is not queued, log error and reject message with requeueing. Everything was valid but couldn't queue job
         if not self.queue_printer(printer_type, filename, gcode):
-            logging.error(f"Failed to queue printer: {printer_type}")
+            logging.error(f"No available printers: {printer_type}")
             ch.basic_reject(delivery_tag=method.delivery_tag, requeue=True)
+            time.sleep(5)
             return
 
         # If everything was successful, acknowledge the message
@@ -83,5 +88,28 @@ class QueueManager:
     
     def queue_printer(self, printer_type: str, filename: str, gcode: str) -> bool:
         # Find available rinter and send gcode and start
-        pass
+        printers: list[str] = self.printer_farm.get_printers(printer_type=printer_type)
+        if not printers:
+            logging.error(f"Printer type {printer_type} not found")
+            return False
         
+        logging.info(f"Available printers: {printers}")
+        
+        for name in printers:
+            try:
+                state = self.printer_farm.get(name, "state")
+
+                if state in [blapi.GcodeState.IDLE, blapi.GcodeState.FINISH]:
+                    printer: PrinterGateway = self.printer_farm.printers.get(name)
+                    file = io.BytesIO(gcode.encode())
+                    file.name = filename
+                    logging.info(f"Uploading gcode to printer {name}")
+                    printer.upload_gcode(file)
+                    file.close()
+                    printer.start_print(filename, 0)
+                    logging.info(f"Started printer {name} with {filename}")
+                    return True
+                    
+            except Exception as e:
+                logging.error(f"Failed to queue printer {name}: {str(e)}")
+                return False
