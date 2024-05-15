d37ea2e2d87ca7df45f19fba81d9d9a1a12a87cd
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2024-05-15 02:13:54 +0100

    refactored starting print to printer farm; replace thread for updating state with availability endpoint

--- printer_queue_manager/src/printer_gateway.py
+++ printer_queue_manager/src/printer_gateway.py
@@ -1,10 +1,8 @@
-from io import BytesIO
 import requests
 import logging
 import zipfile
 import io
 
-import bambulabs_api as blapi
 
 __all__ = ['PrinterGateway']
 
@@ -18,70 +16,15 @@ class PrinterGateway:
     def __init__(self, printer_url, type="p1p"):
         self.printer_url = printer_url
         self.type = type
-        self.data = {}
 
     def __str__(self):
-        return f"PrinterGateway({self.printer_url}, {self.type}, {self.data})"
-
-    def get_remaining_time(self) -> int:
-        """
-        Get the remaining time of the print job
-
-        Returns
-        -------
-        int
-            The remaining time of the print job
-        """
-        response = requests.get(
-            f"http://{self.printer_url}/printer/status/time"
-        )
-        if response.status_code != 200:
-            return -1
-        r = response.json()
-        self.data.update({"time": r['time'] if 'time' in r else -1})
-        return self.data.get("time", -1)
-
-    def get_percentage(self) -> int:
-        """
-        Get the percentage of the print job
-
-        Returns
-        -------
-        int
-            The percentage of the print job
-        """
-        response = requests.get(
-            f"http://{self.printer_url}/printer/status/percentage")
-        if response.status_code != 200:
-            return -1
-        r = response.json()
-        self.data.update(
-            {"percentage": r['percentage'] if 'percentage' in r else -1})
-        return self.data.get("percentage", -1)
-
-    def get_state(self) -> str:
-        """
-        Get the state of the printer
-
-        Returns
-        -------
-        str
-            The state of the printer
-        """
-        url = f"http://{self.printer_url}/printer/status/state"
-        response = requests.get(url)
-        logging.info(f"Update State {url}: {response}")
-        if response.status_code != 200:
-            return "UNKNOWN"
-        r: dict = response.json()
-        self.data.update({"state": blapi.GcodeState(r.get("state", "IDLE"))})
-        return r.get("state", blapi.GcodeState.IDLE)
+        return f"PrinterGateway({self.printer_url}, {self.type})"
 
     def create_zip_archive_in_memory(
-        self, 
-        text_content, 
-        text_file_name='file.txt'):
-        
+            self,
+            text_content,
+            text_file_name='file.txt'):
+
         zip_buffer = io.BytesIO()
         with zipfile.ZipFile(zip_buffer, 'w', zipfile.ZIP_DEFLATED) as zipf:
             zipf.writestr(text_file_name, text_content)
@@ -97,7 +40,7 @@ class PrinterGateway:
         gcode : str
             The gcode to upload
         """
-        
+
         filename += ".3mf"
         gcode_location = "Metadata/plate_1.gcode"
         p = self.create_zip_archive_in_memory(gcode, gcode_location)
@@ -110,7 +53,7 @@ class PrinterGateway:
             raise Exception("Error uploading gcode")
 
         return filename, 1
-        
+
     def start_print(self, filename: str,
                     plater_number: int,
                     use_ams: bool = True,
@@ -129,6 +72,7 @@ class PrinterGateway:
         ams_mapping : list[int]
             AMS trays to use
         """
+        logging.info(f"Starting print on {self.printer_url}")
         response = requests.post(
             f"http://{self.printer_url}/printer/print/start",
             params={"filename": filename,
@@ -139,4 +83,10 @@ class PrinterGateway:
         )
         if response.status_code != 200:
             raise Exception("Error starting print job")
-        self.data.update({"state": blapi.GcodeState.RUNNING})
+
+    def printer_availability(self) -> bool:
+        logging.info(f"Getting availability of printer on {self.printer_url}")
+        response = requests.get(
+            f"http://{self.printer_url}/printer/availability"
+        )
+        return response.json() if response.status_code == 200 else False
