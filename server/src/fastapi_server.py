7ee5035738ad5514f1f4babedb66382f272c8493
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2024-06-26 23:47:18 +0100

    init fastapi server

---
+++ server/src/fastapi_server.py
@@ -0,0 +1,55 @@
+import os
+import secrets
+from typing import Annotated
+
+from fastapi import Depends, FastAPI, HTTPException, Query, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+
+import src.union as union
+
+
+USERNAME = os.getenv("USERNAME").encode()
+PASSWORD = os.getenv("PASSWORD").encode()
+
+app = FastAPI()
+
+security = HTTPBasic()
+
+
+def get_current_username(
+    credentials: Annotated[HTTPBasicCredentials, Depends(security)],
+):
+    current_username_bytes = credentials.username.encode("utf8")
+    correct_username_bytes = USERNAME
+    is_correct_username = secrets.compare_digest(
+        current_username_bytes, correct_username_bytes
+    )
+
+    current_password_bytes = credentials.password.encode("utf8")
+    correct_password_bytes = PASSWORD
+    is_correct_password = secrets.compare_digest(
+        current_password_bytes, correct_password_bytes
+    )
+
+    if not (is_correct_username and is_correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
+
+
+@app.post("/member/add")
+def add_icrs_member(
+    username: Annotated[
+        str, Depends(get_current_username)],
+    id: str = Query(min_length=8, max_length=14),
+    shortcode: str = Query(min_length=3, max_length=7),
+):
+    id = id.upper().strip().replace(" ", "")
+    shortcode = shortcode.lower()
+
+    is_member = union.isMember(shortcode)
+
+    return is_member
