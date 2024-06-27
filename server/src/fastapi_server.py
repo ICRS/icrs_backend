0ad4e43d138ca2e033d061afefe279f792416563
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2024-06-27 01:24:22 +0100

    extracted auth to separate file

--- server/src/fastapi_server.py
+++ server/src/fastapi_server.py
@@ -1,55 +1,65 @@
+import datetime
 import os
-import secrets
 from typing import Annotated
 
-from fastapi import Depends, FastAPI, HTTPException, Query, status
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi import Depends, FastAPI, HTTPException, status
+from pydantic import BaseModel
+import pydantic
 
-import src.union as union
+from database import DB_CONFIG
 
+import psycopg2 as pg
 
-USERNAME = os.getenv("USERNAME").encode()
-PASSWORD = os.getenv("PASSWORD").encode()
+from fast_auth import get_current_username
+
+env = os.getenv("ENV", "dev")
+
+if env == "dev":
+    import union_mock as union
+else:
+    import union as union
+
 
 app = FastAPI()
 
-security = HTTPBasic()
+last_set_time = datetime.datetime.fromtimestamp(0)
+last_short_code = ''
 
 
-def get_current_username(
-    credentials: Annotated[HTTPBasicCredentials, Depends(security)],
-):
-    current_username_bytes = credentials.username.encode("utf8")
-    correct_username_bytes = USERNAME
-    is_correct_username = secrets.compare_digest(
-        current_username_bytes, correct_username_bytes
-    )
-
-    current_password_bytes = credentials.password.encode("utf8")
-    correct_password_bytes = PASSWORD
-    is_correct_password = secrets.compare_digest(
-        current_password_bytes, correct_password_bytes
-    )
-
-    if not (is_correct_username and is_correct_password):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Basic"},
-        )
-    return credentials.username
+class MemberDetails(BaseModel):
+    id: str = pydantic.Field(min_length=8, max_length=14)
+    shortcode: str = pydantic.Field(min_length=3, max_length=7)
+    canPrint: bool = pydantic.Field(True)
+    canLaserCut: bool = pydantic.Field(False)
 
 
 @app.post("/member/add")
 def add_icrs_member(
     username: Annotated[
         str, Depends(get_current_username)],
-    id: str = Query(min_length=8, max_length=14),
-    shortcode: str = Query(min_length=3, max_length=7),
+    member_details: MemberDetails
 ):
-    id = id.upper().strip().replace(" ", "")
-    shortcode = shortcode.lower()
+    id = member_details.id.upper().strip().replace(" ", "")
+    shortcode = member_details.shortcode.lower()
 
     is_member = union.isMember(shortcode)
+    try:
+        with pg.connect(**DB_CONFIG) as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    "INSERT INTO public.access (id, shortcode, " +
+                    "valid, canPrint, canLaserCut) VALUES (%s,%s,%s,%s,%s)",
+                    (id,
+                     shortcode,
+                     is_member,
+                     member_details.canPrint,
+                     member_details.canLaserCut))
 
-    return is_member
+                conn.commit()
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Exception occurred when adding member to database: {e}",
+        )
+
+    return f"Is Member: {is_member}"
