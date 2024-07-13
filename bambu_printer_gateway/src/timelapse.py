26f275babd53de49a7c534f3e9b5e087e746fb40
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2024-07-13 22:19:32 +0100

    update timelapse

--- bambu_printer_gateway/src/timelapse.py
+++ bambu_printer_gateway/src/timelapse.py
@@ -4,34 +4,40 @@ import collections
 from io import BytesIO
 import io
 import logging
+from typing import List
 from PIL import Image
 
 from bambulabs_api import GcodeState, Printer
 
 
 class Timelapse:
-    def __init__(self, printer: Printer) -> None:
+    def __init__(self, printer: Printer, sleep=5) -> None:
         self.state_queue = collections.deque([], maxlen=5)
         self.last_timelapse = []
-        self.current_timelapse_buffer = []
+        self.current_timelapse_buffer: List[Image.Image] = []
         self.printer = printer
+
+        self.timelapse_sleep = sleep
 
     async def timelapse_task(self):
         while True:
-            state = GcodeState(self.printer.get_state())
-            if state == GcodeState.RUNNING:
-                self.current_timelapse_buffer.append(
-                    Image.open(
-                        io.BytesIO(base64.b64decode(
-                            self.printer.get_camera_frame())))
-                )
-                print("adding image")
-            elif state != GcodeState.PAUSE:
-                if self.current_timelapse_buffer:
-                    self.last_timelapse = self.current_timelapse_buffer
-                    self.current_timelapse_buffer = []
+            try:
+                state = GcodeState(self.printer.get_state())
+                if state == GcodeState.RUNNING:
+                    self.current_timelapse_buffer.append(
+                        Image.open(
+                            io.BytesIO(base64.b64decode(
+                                self.printer.get_camera_frame())))
+                    )
+                elif state != GcodeState.PAUSE:
+                    if self.current_timelapse_buffer:
+                        self.last_timelapse = self.current_timelapse_buffer
+                        self.current_timelapse_buffer = []
 
-            await asyncio.sleep(1)
+            except Exception as e:
+                logging.error(f"Error encountered with timelapse: {e}")
+
+            await asyncio.sleep(self.timelapse_sleep)
 
     def get_timelapse(self, timelapse_speed, timelapse_skip):
         if not self.last_timelapse:
