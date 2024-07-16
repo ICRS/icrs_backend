9448a1834d243ab3cbb8022f254bfb0d8f14cf6f
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2024-07-16 18:56:53 +0100

    switch to file writing for timelapse

--- bambu_printer_gateway/src/printer.py
+++ bambu_printer_gateway/src/printer.py
@@ -15,5 +15,6 @@ def get_env_string(env_name: str) -> str:
 HOSTNAME = get_env_string("HOSTNAME")
 ACCESS_CODE = get_env_string("ACCESS_CODE")
 PRINTER_SERIAL = get_env_string("PRINTER_SERIAL")
+PRINTER_NAME = get_env_string("PRINTER_NAME")
 
 printer = Printer(HOSTNAME, ACCESS_CODE, PRINTER_SERIAL)
