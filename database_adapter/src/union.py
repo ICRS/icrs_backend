bdbaf03aeba84d167af87c249206b11f78617bce
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2024-08-24 16:56:49 +0100

    update union to reduce number of queries to api

--- database_adapter/src/union.py
+++ database_adapter/src/union.py
@@ -1,3 +1,5 @@
+import datetime
+import logging
 from icu_ea_api import ICUEActivitiesAPI
 import os
 from dotenv import load_dotenv
@@ -28,21 +30,44 @@ society_api = ICUEActivitiesAPI(CSP_CODE, api_key, year_string)
 # =========================================
 
 
+society_members = society_api.list_members()
+last_update = datetime.datetime.now()
+timeout = datetime.timedelta(seconds=5)
+
+
+def update_members(function):
+    def query_api(*args, **kwargs):
+        global last_update
+        now = datetime.datetime.now()
+
+        if now > last_update + timeout:
+            global society_members
+            society_members = society_api.list_members()
+            last_update = now
+            logging.debug("Updated society members list")
+
+        return function(*args, **kwargs)
+    return query_api
+
+
+@update_members
 def isMember(shortcode: str) -> bool:
     return shortcode in [member['Login']
-                         for member in society_api.list_members()]
+                         for member in society_members]
 
 
+@update_members
 def getShortcodesToCIDAndName(shortcodes) -> list:
     d = {member['Login']: (f"{member['FirstName']} {member['Surname']}",
                            member['CID'], member['Login'])
-         for member in society_api.list_members()}
+         for member in society_members}
     return list(set(d[s] for s in shortcodes if s in d))
 
 
+@update_members
 def is_member_list(shortcodes: list[str]) -> bool:
     try:
-        members = set(member['Login'] for member in society_api.list_members())
+        members = set(member['Login'] for member in society_members)
         shortcodes = [code for code in shortcodes if code in members]
         return shortcodes
     except Exception:
