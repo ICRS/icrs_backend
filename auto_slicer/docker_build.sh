26a58bd68936ecb379d8f53c19cfeeec28841cc8
Etienne Naude <eti@naude.dev>
2024-09-06 20:45:38 +0100

    docker builds

--- auto_slicer/docker_build.sh
+++ auto_slicer/docker_build.sh
@@ -1,1 +1,1 @@
-docker buildx build --builder=builder --push --platform linux/amd64 -t localhost:32000/auto-slicer .
+docker buildx build --builder=builder --push --platform linux/amd64 -t localhost:32000/auto-slicer:$1 .
