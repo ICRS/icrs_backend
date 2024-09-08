05feebbb4ed7256333f7ebd94faf671c008f80c5
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2024-09-08 23:55:21 +0100

    update db adapter and slicer endpoint

--- database_adapter/src/slicer.py
+++ database_adapter/src/slicer.py
@@ -3,12 +3,10 @@ __all__ = ["slicer_router"]
 import datetime
 import logging
 
-from typing import Annotated
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, HTTPException, Query, status
 
 from src.database import main_db_pool
-
-from src.auth import get_current_username
+from src.validation import SHORTCODE_REGEX
 
 
 slicer_router = APIRouter(prefix="/slicer", tags=["Slicer"])
@@ -16,30 +14,38 @@ slicer_router = APIRouter(prefix="/slicer", tags=["Slicer"])
 
 @slicer_router.get("/print/permissions")
 def print_permissions(
-    username: Annotated[str, Depends(get_current_username)],
-    time: datetime.timedelta
+    # username: Annotated[str, Depends(get_current_username)],
+    time: datetime.timedelta,
+    shortcode: str = Query(pattern=SHORTCODE_REGEX)
 ) -> str:
     try:
         with main_db_pool.connection() as conn:
             with conn.cursor() as cur:
                 now = datetime.datetime.now()
+                print_minutes = time.seconds // 60
+                logging.warning(print_minutes)
                 if 7 < now.hour < 22:
                     cur.execute(
                         "SELECT TRUE FROM public.induction i WHERE "
-                        "i.canprint and i.valid and i.print_daytime_time < %s",
+                        "i.canprint and i.valid and i.print_daytime_time > %s "
+                        "and i.shortcode=%s",
                         (
-                            time.seconds // 60
+                            print_minutes,
+                            shortcode
                         )
                     )
                 else:
                     cur.execute(
                         "SELECT TRUE FROM public.induction i WHERE "
-                        "i.canprint and i.valid and i.print_night_time < %s",
+                        "i.canprint and i.valid and i.print_night_time > %s "
+                        "and i.shortcode=%s",
                         (
-                            time.seconds // 60
+                            time.seconds // 60,
+                            shortcode
                         )
                     )
                 result = cur.fetchone()
+                logging.info(result)
 
     except Exception as e:
         msg = f"Error getting permissions for printer {e}"
@@ -52,6 +58,6 @@ def print_permissions(
     if not result:
         msg = "Member cannot not print at this time"
         logging.info(msg)
-        raise HTTPException(status_code=status.HTTP_302_FOUND,
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                             detail=msg)
     return True
