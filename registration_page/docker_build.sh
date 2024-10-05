e83a14e99f29c16556cee4d3a05291019e4ffb34
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2024-10-05 20:28:20 +0100

    add pipeline for registration page

--- registration_page/docker_build.sh
+++ registration_page/docker_build.sh
@@ -1,4 +1,4 @@
 # docker build -t icrs-registration-page .
 # docker push localhost:32000/icrs-registration-page
-docker buildx build --builder=builder --push --platform linux/arm64,linux/amd64 -t localhost:32000/icrs-registration-page .
+docker buildx build --builder=builder --build-arg ENV=${2:-dev} --push --platform linux/arm64,linux/amd64 -t localhost:32000/icrs-registration-page:${1:-latest} .
 
