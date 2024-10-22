0a666b0e5f61f4a611a8485aa7a9a057fda89adf
Chris Ioannidis <142539198+acse-ci223@users.noreply.github.com>
2024-10-22 22:53:14 +0100

    Copied service

---
+++ notion_manager/src/server.py
@@ -0,0 +1,44 @@
+import logging
+from fastapi import APIRouter, Query
+import requests
+import json
+
+router = APIRouter(
+    tags=["server"],
+)
+
+# ==============================================================================
+PRINTER_CONFIG = json.load(open("printer_config.json"))
+
+PRINTER_NAMES = list(PRINTER_CONFIG["PRINTER_NAMES"])
+PRINTER_GATEWAY_ENDPOINT_SUFFIX = str(
+    PRINTER_CONFIG["PRINTER_GATEWAY_ENDPOINT_SUFFIX"])
+
+# ==============================================================================
+
+
+@router.post("/availability")
+async def update_availability(
+    uid: str = "",
+    printer_name: str = Query(enum=PRINTER_NAMES),
+    available: bool = False,
+):
+    logging.info(f"Updating availability for {uid} to {available}")
+    uid = uid.strip().replace(" ", "").rjust(8, "0")
+    r = requests.post(
+        f"http://{printer_name}{PRINTER_GATEWAY_ENDPOINT_SUFFIX}/printer/available", # noqa
+        params={"uid": uid, "available": available},
+    )
+
+    return r.status_code
+
+
+@router.get("/availability")
+async def get_availability(
+    printer_name: str = Query(enum=PRINTER_NAMES),
+):
+    r = requests.get(
+        f"http://{printer_name}{PRINTER_GATEWAY_ENDPOINT_SUFFIX}/printer/available", # noqa
+    )
+
+    return r.json()
