0bce48de834f8c1d8a7240967ae2a83957f036e6
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2024-10-25 20:08:08 +0100

    update ci files

--- notion_manager/docker_build.sh
+++ notion_manager/docker_build.sh
@@ -1,1 +1,1 @@
-docker buildx build --builder=builder --push --platform linux/arm64,linux/amd64 -t localhost:32000/notion_manager .
+docker buildx build --builder=builder --push --platform linux/arm64,linux/amd64 -t localhost:32000/notion_manager:${1:-latest} .
