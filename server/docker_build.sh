ad45c4a4904c678e327a643280362eb8ee922712
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2025-01-03 23:16:18 +0000

    init migration to ingress for access service

--- server/docker_build.sh
+++ server/docker_build.sh
@@ -1,3 +1,1 @@
-#docker build -t localhost:32000/icrs-access .
-#docker push localhost:32000/icrs-access
-docker buildx build --builder=builder --push --platform linux/arm64,linux/amd64 -t localhost:32000/icrs-access:$1 .
+docker buildx build --builder=builder --push --platform linux/arm64,linux/amd64 -t localhost:32000/icrs-access:${1:-latest} .
