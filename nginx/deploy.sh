7565f5c169ab47217bf1ce61a072fb9fe3347446
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2025-01-05 16:23:16 +0000

    add github actions for nginx

--- nginx/deploy.sh
+++ nginx/deploy.sh
@@ -1,1 +1,2 @@
 kubectl create configmap nginx-conf -n ingress --from-file nginx-conf=nginx-prod.conf --dry-run=client -o yaml | kubectl apply -f -
+kubectl rollout restart -n ingress deployment nginx-ingress-controller
