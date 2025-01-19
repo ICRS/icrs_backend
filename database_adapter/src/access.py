59ce4c4d5c12b58ca8537e83a1edd5fc993721d3
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2025-01-19 22:10:04 +0000

    update access for print window

--- database_adapter/src/access.py
+++ database_adapter/src/access.py
@@ -1,5 +1,7 @@
+import math
 from typing import Annotated
 from fastapi import APIRouter, Depends, Query
+from pydantic import BaseModel
 
 from src.auth import get_current_username
 from src.database import main_db_pool
@@ -15,7 +17,70 @@ def update_print_window(
     with main_db_pool.connection() as conn:
         with conn.cursor() as cur:
             cur.execute(
-                "INSERT INTO public.card_scan_log(id) "
-                "VALUES (%s)",
-                (uuid,),
+                "INSERT INTO public.card_scan_log(ID, VALID) "
+                "SELECT card_id, coalesce(fiw.canprint, FALSE) as VALID "
+                "FROM public.full_induction_view fiw "
+                "WHERE card_id=%s "
+                "LIMIT 1 "
+                "RETURNING VALID",
+                (uuid, ),
             )
+            return bool(cur.fetchall())
+
+
+@access_router.get("/print-window/valid")
+def get_print_window_valid(
+        username: Annotated[str, Depends(get_current_username)],
+        time_s: int = Query(default=60, min=0, max=3600),
+):
+    with main_db_pool.connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                "SELECT shortcode FROM public.card_scan_log l "
+                "INNER JOIN public.full_induction_view sc "
+                "ON l.id=sc.card_id "
+                "WHERE l.scanned_time > CURRENT_TIMESTAMP - (%s ||' seconds')::interval "
+                "ORDER BY l.scanned_time DESC "
+                "LIMIT 1",
+                (time_s,),
+            )
+
+            return bool(cur.fetchall())
+
+
+class PrintData(BaseModel):
+    time: str
+    weight: str
+    name: str
+
+
+@access_router.post("/print-metric")
+def post_print_time(
+    print_data: PrintData
+):
+    def parse_to_int(s: str) -> int:
+        '''Expects the time to be in in seconds (float)'''
+        return math.ceil(float(s))
+
+    print_time = parse_to_int(print_data.time.strip())
+    print_weight = parse_to_int(print_data.weight.strip())
+    printer_name = print_data.name.strip()
+
+    with main_db_pool.connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                "INSERT INTO public.print_metrics (shortcode, "
+                "print_duration, print_weight, printer_name) "
+                "SELECT shortcode, %s, %s, %s FROM ("
+                "SELECT shortcode, l.valid "
+                "FROM public.card_scan_log l "
+                "INNER JOIN public.full_induction_view sc "
+                "ON l.id=sc.card_id "
+                "ORDER BY l.scanned_time DESC "
+                "LIMIT 1) "
+                "WHERE valid "
+                "RETURNING shortcode",
+                (print_time, print_weight, printer_name)
+            )
+
+            return [i[0] for i in cur.fetchall()]
