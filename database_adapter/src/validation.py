e53efe32abc1db3d9054345003aca52a094affd7
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2025-03-02 00:16:53 +0000

    update validation

--- database_adapter/src/validation.py
+++ database_adapter/src/validation.py
@@ -1,2 +1,22 @@
+__all__ = [
+    "SHORTCODE_REGEX",
+    "DISCORD_ID_REGEX",
+    "SHORTCODE_QUERY",
+    "SHORTCODE_QUERY_PYDANTIC",
+]
+
+from fastapi import Query
+import pydantic
+
+
 SHORTCODE_REGEX = r'^[a-z]{2,3}[0-9]{2,5}$'
 DISCORD_ID_REGEX = r'^<@[0-9]{17,21}>$'
+
+SHORTCODE_QUERY = Query(
+    pattern=SHORTCODE_REGEX,
+    min_length=3,
+    max_length=10)
+SHORTCODE_QUERY_PYDANTIC = pydantic.Field(
+    pattern=SHORTCODE_REGEX,
+    min_length=3,
+    max_length=10)
