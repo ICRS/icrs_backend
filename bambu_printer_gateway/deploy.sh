c229893e6408bde0665446ae5375da07f148bd03
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2025-05-16 22:16:17 +0100

    remove deployment files

--- bambu_printer_gateway/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PRINTERS=("andrew-printerson" "eric-printman" "printer-cheung" "freddy-printer" "printy-mcprintface" "additive-spiers")
-
-for printer in ${PRINTERS[@]}; do
-    echo "Deploying printer $printer"
-    cat deployment.yaml | sed -e "s/\${printer-name}/$printer/g" | sed -e "s/latest/${1:-latest}/g" | kubectl apply -f -
-    # Deploy printer
-done
